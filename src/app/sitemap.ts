ddd35d1287d7ece5d49bd8d699c61fd0556183db
Taek Lim <tlim41@gatech.edu>
2024-10-29 20:46:20 +0700

    fix: sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,29 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from 'next';
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    // {
-    //   url: 'https://acme.com',
-    //   lastModified: new Date(),
-    //   changeFrequency: 'yearly',
-    //   priority: 1,
-    // },
-    // {
-    //   url: 'https://acme.com/about',
-    //   lastModified: new Date(),
-    //   changeFrequency: 'monthly',
-    //   priority: 0.8,
-    // },
-    // {
-    //   url: 'https://acme.com/blog',
-    //   lastModified: new Date(),
-    //   changeFrequency: 'weekly',
-    //   priority: 0.5,
-    // },
-  ]
+    return [
+        {
+          url: 'https://intothedeep.github.com',
+          lastModified: new Date(),
+          changeFrequency: 'yearly',
+          priority: 1,
+        },
+        // {
+        //   url: 'https://acme.com/about',
+        //   lastModified: new Date(),
+        //   changeFrequency: 'monthly',
+        //   priority: 0.8,
+        // },
+        // {
+        //   url: 'https://acme.com/blog',
+        //   lastModified: new Date(),
+        //   changeFrequency: 'weekly',
+        //   priority: 0.5,
+        // },
+    ];
 }
 
-  // use next fetch to deduplicate requests
+// use next fetch to deduplicate requests
 // private static HttpAgent = {
 // get: async <T>(path: string, params: { params: object }): Promise<{ data: T }> => {
 //     const queryParams = qs.stringify(params.params, { arrayFormat: 'indices', encodeValuesOnly: true });
@@ -40,3 +40,28 @@ export default function sitemap(): MetadataRoute.Sitemap {
 //     return { data: response };
 // },
 // };
+
+// sitemap.xml.js
+
+// export const dynamic = 'force-static'; // Ensures this page is treated as static
+// export const revalidate = 60; // Revalidate every 60 seconds
+
+// export default async function handler(req, res) {
+//     const sitemap = generateSitemap(); // Replace with your sitemap generation logic
+//     res.setHeader('Content-Type', 'application/xml');
+//     res.write(sitemap);
+//     res.end();
+// }
+
+// function generateSitemap() {
+//     // Implement sitemap generation logic here
+//     return `<?xml version="1.0" encoding="UTF-8"?>
+//   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+//     <url>
+//       <loc>https://example.com/</loc>
+//       <lastmod>${new Date().toISOString()}</lastmod>
+//       <changefreq>daily</changefreq>
+//       <priority>1.0</priority>
+//     </url>
+//   </urlset>`;
+// }
